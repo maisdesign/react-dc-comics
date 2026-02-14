51b2865b904b44d7767461b7340ce984b6cc784d
Maisdesign <downloadtaky@gmail.com>
2026-02-14 17:06:29 +0100

    chore: wrap Logo component in a div for improved structure and update header CSS for alignment

--- react-dc-comics/src/components/header/Logo.jsx
+++ react-dc-comics/src/components/header/Logo.jsx
@@ -1,9 +1,9 @@
 import logoImg from '../../assets/dc-logo.png'
 function Logo() {
     return (
-        <>
+        <div className="logo">
             <img src={logoImg} alt="Logo Dc Comics" className="logo-img" />
-        </>
+        </div>
     )
 }
 export default Logo
