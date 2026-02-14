7d09014363cc64f2026f3393af1d621532f8f8a7
Maisdesign <downloadtaky@gmail.com>
2026-02-14 15:51:18 +0100

    chore: restructure footer links layout and update CSS for improved styling

--- react-dc-comics/src/components/footer/FooterLinks.jsx
+++ react-dc-comics/src/components/footer/FooterLinks.jsx
@@ -7,38 +7,44 @@ function FooterLinks() {
     return (
         <div className="footerlinks">
             <div className="footerlinks-container">
-                <ul className="ul-title">DC COMICS
-                    {dcComicsLinks.map(({ id, label, link }) => (
-                        <li key={id} className="nav-link">
-                            <a href={link}>{label}</a>
-                        </li>
-                    )
-                    )}
-                </ul>
-                <ul className="ul-title">SHOP
-                    {shopLinks.map(({ id, label, link }) => (
-                        <li key={id} className="nav-link">
-                            <a href={link}>{label}</a>
-                        </li>
-                    )
-                    )}
-                </ul>
-                <ul className="ul-title">DC
-                    {dcLinks.map(({ id, label, link }) => (
-                        <li key={id} className="nav-link">
-                            <a href={link}>{label}</a>
-                        </li>
-                    )
-                    )}
-                </ul>
-                <ul className="ul-title">SITES
-                    {siteLinks.map(({ id, label, link }) => (
-                        <li key={id} className="nav-link">
-                            <a href={link}>{label}</a>
-                        </li>
-                    )
-                    )}
-                </ul>
+                <div className="footerlinks-row-1">
+                    <ul className="ul-title">DC COMICS
+                        {dcComicsLinks.map(({ id, label, link }) => (
+                            <li key={id} className="nav-link">
+                                <a href={link}>{label}</a>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                    <ul className="ul-title">SHOP
+                        {shopLinks.map(({ id, label, link }) => (
+                            <li key={id} className="nav-link">
+                                <a href={link}>{label}</a>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </div>
+                <div className="footerlinks-row-2">
+                    <ul className="ul-title">DC
+                        {dcLinks.map(({ id, label, link }) => (
+                            <li key={id} className="nav-link">
+                                <a href={link}>{label}</a>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </div>
+                <div className="footerlinks-row-3">
+                    <ul className="ul-title">SITES
+                        {siteLinks.map(({ id, label, link }) => (
+                            <li key={id} className="nav-link">
+                                <a href={link}>{label}</a>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </div>
             </div>
         </div>
     )
