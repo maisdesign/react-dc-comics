450ce77bc0b97ae57218914db04ce79bdba79686
Maisdesign <downloadtaky@gmail.com>
2026-02-16 12:01:16 +0100

    chore: restructure SocialLinks component for improved layout; enhance visual hierarchy with a header

--- react-dc-comics/src/components/footer/SocialLinks.jsx
+++ react-dc-comics/src/components/footer/SocialLinks.jsx
@@ -4,13 +4,17 @@ import CtaCard from './cta/CtaCard.jsx'
 function SocialLinks() {
     return (
 
-        <ul className="cta-links-container">
-            {socialLinks.map(({ id, label, link, imgurl }) => (
-                <CtaCard key={id} id={id} label={label} link={link} imgurl={imgurl} />
-            )
-            )}
-        </ul>
+        <div className="social-zone">
 
+            <div className="social-cta"><p>FOLLOW US</p></div>
+            <ul className="cta-links-container">
+                {socialLinks.map(({ id, label, link, imgurl }) => (
+                    <CtaCard key={id} id={id} label={label} link={link} imgurl={imgurl} />
+                )
+                )}
+            </ul>
+
+        </div>
     )
 }
 export default SocialLinks
