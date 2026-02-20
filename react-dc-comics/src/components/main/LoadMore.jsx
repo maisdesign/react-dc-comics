ee14459e2a160448ecb9e6a5bc74912e6bd4d661
Maisdesign <downloadtaky@gmail.com>
2026-02-20 18:58:39 +0100

    Aggiunti nuovi fumetti, reso funzionante LoadMore che ora cicla tra i fumetti, se finissero ricomincia da capo

--- react-dc-comics/src/components/main/LoadMore.jsx
+++ react-dc-comics/src/components/main/LoadMore.jsx
@@ -1,8 +1,19 @@
-function LoadMore() {
+
+function LoadMore({ setComicSlice, comicSlice, setComicStart, comicStart, comics }) {
+
+    function resetLoadMore() {
+        if (comicSlice >= comics.length) {
+            setComicStart(0);
+            setComicSlice(12);
+        } else {
+            setComicSlice(comicSlice + 6); setComicStart(comicStart + 6);
+        }
+    }
     return (
         <>
             <div className="loadmore-container mx-auto col-2 my-3">
-                <button className="loadmore-button">Load More</button>
+
+                <button className="loadmore-button" onClick={() => { resetLoadMore(); }}>Load More</button>
             </div>
         </>
     )
--- react-dc-comics/src/components/main/Main.jsx
+++ react-dc-comics/src/components/main/Main.jsx
@@ -2,8 +2,11 @@ import Jumbotron from './Jumbotron.jsx'
 import ShowcaseCard from './ShowcaseCard.jsx'
 import comics from '../../data/comics.js'
 import LoadMore from './LoadMore.jsx'
+import { useState } from 'react'
 
 function Main() {
+    const [comicSlice, setComicSlice] = useState(12)
+    const [comicStart, setComicStart] = useState(0)
     return (
         <>
             <div className="jumbotron-wrapper">
@@ -12,12 +15,12 @@ function Main() {
             <main className="container">
                 <ul className="showcase-list d-flex flex-wrap list-unstyled row mx-auto container py-3 px-0">
 
-                    {comics.map(({ id, title, thumb, series }) => (
+                    {comics.slice(comicStart, comicSlice).map(({ id, title, thumb, series }) => (
                         <ShowcaseCard key={id} id={id} title={title} series={series} thumb={thumb} />
                     )
                     )}
                 </ul>
-                <LoadMore />
+                <LoadMore comicSlice={comicSlice} setComicSlice={setComicSlice} comicStart={comicStart} setComicStart={setComicStart} comics={comics} />
             </main>
         </>
     )
