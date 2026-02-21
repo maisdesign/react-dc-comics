16b9836bc6c1360f69c74a956c635257a86e67d1
Maisdesign <downloadtaky@gmail.com>
2026-02-21 17:28:34 +0100

    Sistemato breakpoint mobile

--- react-dc-comics/src/components/footer/BottomFooter.jsx
+++ react-dc-comics/src/components/footer/BottomFooter.jsx
@@ -4,7 +4,7 @@ import SocialLinks from './SocialLinks.jsx'
 function BottomFooter() {
     return (
         <div className="bottom-footer">
-            <div className="container d-flex justify-content-between align-items-center py-3">
+            <div className="container d-flex justify-content-between align-items-center py-3 flex-md-row flex-column">
                 <SignupButton />
                 <SocialLinks />
             </div>
