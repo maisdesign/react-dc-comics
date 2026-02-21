cce83171c460adf83f51622170c2ecbe2d6cb694
Maisdesign <downloadtaky@gmail.com>
2026-02-21 17:17:59 +0100

    Aggiunto lazyload ad alcune immagini che non sono above the fold

--- react-dc-comics/src/components/main/ShowcaseCard.jsx
+++ react-dc-comics/src/components/main/ShowcaseCard.jsx
@@ -3,7 +3,7 @@ function ShowcaseCard({ title, thumb, series }) {
 
         <li className="showcomic gap-2 col-sm-6 col-md-4 col-lg-2 py-3">
             <div className="singlecomic-container">
-                <div className="singlecomic-img"><a href='#'><img src={thumb} alt={title} className="img-fluid" /></a></div>
+                <div className="singlecomic-img"><a href='#'><img loading="lazy" src={thumb} alt={title} className="img-fluid" /></a></div>
                 <div className="singlecomic-series py-2">
                     <a href='#' className="text-white text-decoration-none">{series}</a>
                 </div>
